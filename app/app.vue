98c1aee296844e33404928802ff7caf333baf680
Matheus Victor Martins <matheusvictor.profissional@gmail.com>
2026-04-17 13:09:56 -0300

    Rick and morty api test

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<div class="flex w-full">
+			<div v-for="(item, index) in data.results" :key="index">
+				<img :src="item.image" alt="" />
+				<p>{{ item.name }}</p>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script lang="ts" setup>
+const { data, status, error, refresh, clear } = await useFetch(
+	"https://rickandmortyapi.com/api/character",
+);
+console.log(data.value);
+</script>
